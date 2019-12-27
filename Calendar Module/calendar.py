46cbd84a9a2c159a23fe4bc5f69639da9d3b583e
Sawrav Chowdhury <43489196+sawravchy@users.noreply.github.com>
2019-12-28 02:01:25 +0600

    Create calendar.py

---
+++ Calendar Module/calendar.py
@@ -0,0 +1,20 @@
+import calendar
+#print(calendar.weekheader(4))
+#print(calendar.firstweekday())
+print()
+#print(calendar.month(2019,12,4)) #(year,month,weekday name character)
+year = int(input("Enter The Year :"))
+
+print(calendar.calendar(year,3))
+is_leap = calendar.isleap(year)
+if is_leap == True:
+  print("This year is leap year")
+else:
+  print(f"Note: {year} is not Leap Year")
+  print()
+
+
+
+leap_days = calendar.leapdays(2000,year)
+print(f"Fun Fact: From 2000 to {year} there are {leap_days} Leap Days :) " )
+print()
