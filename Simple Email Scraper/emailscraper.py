2de0ec7ad7d7cdf88bf1f5965bb5b58b232aacb4
Rizwan Hasan <rizwan.hasan486@gmail.com>
2020-04-23 23:01:17 +0600

    Basic Issue Fixed

--- Simple Email Scraper/emailscraper.py
+++ Simple Email Scraper/emailscraper.py
@@ -1,13 +1,15 @@
 import re
-print("A simple online email extractor by python")
-print()
+
+print("A simple online email extractor by python\n")
 text = input("Please enter your text here:")
-while text in [""," "]:
-  text = input("\nText field is empty please enter your text here:")
+
+while text in ["", " "]:
+    text = input("\nText field is empty please enter your text here:")
 else:
-  pattern = re.compile("[a-zA-Z0-9\.\_\-]+[@]+[a-zA-Z0-9]+\.[a-zA-Z]+")
-  result =pattern.findall(text)
-if result == []:
-  print("Sorry we didn't find any email in your text.")
-else:  
-  print(f"\nList Of Email Address: {result}")
+    pattern = re.compile("[a-zA-Z0-9\.\_\-]+[@]+[a-zA-Z0-9]+\.[a-zA-Z]+")
+    result = pattern.findall(text)
+
+if result:
+    print(f"\nList Of Email Address: {result}")
+else:
+    print("Sorry we didn't find any email in your text.")
